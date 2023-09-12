fd361763bab7ccb572fd0d83be0fb95acfb17bf5
Ibrahim Bashir <ibrahimembash@gmail.com>
2023-09-12 17:19:36 +0200

    add feature 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center  p-24">
      Hello Main
+     <h3>This is a new Feature 1</h3>
     </main>
   )
 }
