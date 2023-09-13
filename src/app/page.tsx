f468ab230a4676c55b291b3a14257ecffd0a62ec
Bashir Ibrahim Muhammad (Embash) <ibrahimembash@gmail.com>
2023-09-13 17:56:51 +0200

    Revert "h2 (#11)"
    This reverts commit 4e618e1dd50dd1e6fc6ef022d4770dba67f3f858.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center  p-24">
      Hello Main
      <h3>This is a Feature fix</h3>
-     <h2>h2 feature added </h2>
     </main>
   )
 }
